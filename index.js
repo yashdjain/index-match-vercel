00e46e47c7ff3d505284c98429de57488b562bbb
Yash Jain <yashjain@Yashs-MacBook-Pro.local>
2023-04-13 17:05:07 -0700

    Add accounts router

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Import packages
 const express = require("express");
 const home = require("./routes/home");
+const accounts = require("./routes/accounts")
 
 // Middlewares
 const app = express();
@@ -8,6 +9,7 @@ app.use(express.json());
 
 // Routes
 app.use("/home", home);
+app.use("/accounts", accounts)
 
 // connection
 const port = process.env.PORT || 9001;
